dbbbed1e53fe6b22e8607bb3c0abb289707a5c04
admin@atul <atulsingh29@protonmail.com>
2020-06-24 11:25:21 +0530

    minor fixes

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,HttpResponse
 
+
+
 def blog(request):
-    return HttpResponse("<h2> blog is under development, it will be available soon.</h2><p>Stay tuned for quality content on tech</p>")
+    return HttpResponse('Under Development')
+
