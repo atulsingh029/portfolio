71fca015dc48d8c7255652c274d3beef0f198b8c
admin@atul <atulsingh29@protonmail.com>
2020-06-28 19:49:16 +0530

    minor fixes

--- home/forms.py
+++ home/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 
 class MailingForm(forms.Form):
-    name = forms.CharField(max_length=160,required=True,label='Name',widget=forms.TextInput(attrs={ 'size': '35',}))
-    email = forms.EmailField(max_length=255,required=True,label='Email',widget=forms.EmailInput(attrs={ 'size': '35',}))
-    subject = forms.CharField(max_length=512,required=True,label='Subject',widget=forms.TextInput(attrs={ 'size': '35',}))
+    name = forms.CharField(max_length=160,required=True,label='Name',widget=forms.TextInput(attrs={ 'size': '30',}))
+    email = forms.EmailField(max_length=255,required=True,label='Email',widget=forms.EmailInput(attrs={ 'size': '30',}))
+    subject = forms.CharField(max_length=512,required=True,label='Subject',widget=forms.TextInput(attrs={ 'size': '30',}))
     message = forms.CharField(max_length=1024,required=True,label='Message',widget=forms.Textarea())
 
 
 class VisitorForm(forms.Form):
-    name = forms.CharField(min_length=3, max_length=160, required=True, label="Name",widget=forms.TextInput(attrs={ 'size': '30',}))
-    email = forms.EmailField(max_length=254, required=True, label="Email",widget=forms.EmailInput(attrs={ 'size': '30',}))
+    name = forms.CharField(min_length=3, max_length=160, required=True, label="Name",)
+    email = forms.EmailField(max_length=254, required=True, label="Email",)
     subscribe = forms.BooleanField(required=False, label="Subscribe To My Newsletter ")
